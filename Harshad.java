ddb316bb9c65e6473ce413b84f7dcd404d7355f6
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-14 21:34:07 +0530

    Add comments

--- Harshad.java
+++ Harshad.java
@@ -2,35 +2,35 @@ import java.util.*;
 
 public class Harshad {
     Scanner sc = new Scanner(System.in);
-    int n, m;
+    int n, m; // instance vars
 
-    Harshad() {
+    Harshad() { // initialize vars
         n = 0;
         m = 0;
     }
 
     boolean isHarshad(int x) {
-        int sum = 0;
-        int t = x;
-        while (t != 0) {
+        int sum = 0; // reset sum
+        int t = x; // store x temporarily for calcs
+        while (t != 0) { // calc sum of digits
             int d = t % 10;
             sum = sum + d;
             t = t / 10;
         }
 
-        return x % sum == 0;
+        return x % sum == 0; // return whether harshad
     }
 
     void acc_calc() {
         System.out.println("Enter a number:");
-        n = sc.nextInt();
+        n = sc.nextInt(); // accept number
 
         if (isHarshad(n)) {
             System.out.println(n + " is Harshad");
             System.out.println("Enter another number:");
-            m = sc.nextInt();
+            m = sc.nextInt(); // accept another number ONLY if the first number is harshad
 
-            boolean found = false;
+            boolean found = false; // set found false by default
             System.out.println("Harshad numbers between " + n + " and " + (n + m) + " are:");
             for (int i = n + 1; i < n + m; i++) {
                 n = i;
@@ -48,7 +48,7 @@ public class Harshad {
     }
 
     public static void main(String[] args) {
-        Harshad obj = new Harshad();
+        Harshad obj = new Harshad(); // call method funcs
         obj.acc_calc();
     }
 }
